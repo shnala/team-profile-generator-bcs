3e6fe286d63ad11e85d7d7eb7bc497ee7eba8a7a
Shawn Analla <shawnanalla@gmail.com>
2022-01-26 16:52:13 -0800

    user inputs work, need to generate html

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer')
+const generateHtml = require('./util/generateHtml')
 const Employee = require('./lib/Employee')
 const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
@@ -32,7 +33,7 @@ const start = () => {
         },
     ])
     .then((response) => {
-        const manager = new Manager(response);
+        const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
         employees.push(manager);
         console.log(employees);
         nextQuestion();
@@ -46,7 +47,7 @@ const nextQuestion = () => {
         {
             type: 'list',
             message: "Next, please choose to add an engineer or intern to your team roster.",
-            choices: ["Add Engineer", "Add Intern"],
+            choices: ["Add Engineer", "Add Intern", "Add Employee", "Finish"],
             name: 'choice'
         }
     ])
@@ -58,8 +59,13 @@ const nextQuestion = () => {
             case 'Add Intern':
                 addIntern();
                 break;
+            case 'Add Employee':
+                addEmployee();
+                break;
             default:
-                console.log("???");
+                console.log(employees);
+                //TODO: Will call the html generator HERE.
+                // generateHtml();
         }
     })
 
@@ -70,31 +76,97 @@ const addEngineer = () => {
     .prompt([
         {
             type: 'input',
-            message: "Welcome to team profile generator! You will be introduced to a series of prompts that will be used to generate your HTML file. To begin, please enter your team manager's name.",
+            message: "Please enter the name of your engineer.",
             name: 'name'
         },
         {
             type: 'input',
-            message: "Please enter your team manager's integer ID.",
+            message: "Please enter your engineer's integer ID.",
             name: 'id'
         },
         {
             type: 'input',
-            message: "Please enter your team manager's email.",
+            message: "Please enter your engineer's email.",
             name: 'email'
         },
         {
             type: 'input',
-            message: "Lastly, please enter your team manager's office number.",
-            name: 'officeNumber'
+            message: "Lastly, please enter your engineer's GitHub username.",
+            name: 'github'
         },
     ])
+    .then((response) => {
+        const engineer = new Engineer(response.name, response.id, response.email, response.github);
+        employees.push(engineer);
+        console.log(employees);
+        nextQuestion();
+    })
 
 }
 
+const addIntern = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "Please enter the name of your intern.",
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: "Please enter your intern's integer ID.",
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: "Please enter your intern's email.",
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: "Lastly, please enter your the name of your intern's school.",
+            name: 'school'
+        },
+    ])
+    .then((response) => {
+        const intern = new Intern(response.name, response.id, response.email, response.school);
+        employees.push(intern);
+        console.log(employees);
+        nextQuestion();
+    })
+}
 
+const addEmployee = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "Please enter the name of your employee.",
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: "Please enter your employee's integer ID.",
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: "Please enter your employee's email.",
+            name: 'email'
+        }
+    ])
+    .then((response) => {
+        const employee = new Employee(response.name, response.id, response.email);
+        employees.push(employee);
+        console.log(employees);
+        nextQuestion();
+    })
+
+}
 
 start();
+
+// generateHtml.generateTeam();
 
 //use inquirer to take in user input on employees
 //on start, user is prompted to enter TEAM MANAGER's name, employee id, email, and office number.
